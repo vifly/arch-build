6568266497d58b72a6c28b832d6db2aa5b39d831
Vifly <viflythink@gmail.com>
2021-11-11 19:50:33 +0800

    fix path error

--- build-aur-action/entrypoint.sh
+++ build-aur-action/entrypoint.sh
@@ -15,6 +15,4 @@ pacman-key --init
 pacman -Sy --noconfirm && pacman -S --noconfirm archlinuxcn-keyring
 pacman -S --noconfirm yay
 
-sudo --set-home -u builder yay -S --noconfirm "$pkgname"
-mv ~/.cache/yay/* ./
-
+sudo --set-home -u builder yay -S --noconfirm --builddir=./ "$pkgname"

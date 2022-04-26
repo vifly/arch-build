ecb6918f04c4034da14b6e56cef8a1225f524e7c
Vifly <viflythink@gmail.com>
2022-04-26 22:24:20 +0800

    fix permission error

--- build-nonaur-action/entrypoint.sh
+++ build-nonaur-action/entrypoint.sh
@@ -4,21 +4,21 @@ set -euo pipefail
 FILE="$(basename "$0")"
 
 # Enable the multilib repository
-cat << EOM >> /etc/pacman.conf
+sudo cat << EOM >> /etc/pacman.conf
 [multilib]
 Include = /etc/pacman.d/mirrorlist
 EOM
 
-pacman -Syu --noconfirm --needed base-devel
+sudo pacman -Syu --noconfirm --needed base-devel
 
 # Makepkg does not allow running as root
 # Create a new user `builder`
 # `builder` needs to have a home directory because some PKGBUILDs will try to
 # write to it (e.g. for cache)
-useradd builder -m
+sudo useradd builder -m
 # When installing dependencies, makepkg will use sudo
 # Give user `builder` passwordless sudo access
-echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+sudo echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
 # Give all users (particularly builder) full access to these files
 chmod -R a+rw .

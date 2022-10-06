4ddd0d700efc361b6e292d9559aa1a0361d7a17b
Vifly <viflythink@gmail.com>
2022-10-06 13:25:21 +0800

    add support for install pkgs before build

--- build-aur-action/entrypoint.sh
+++ build-aur-action/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 pkgname=$1
+preinstall_pkgs=$2
 
 useradd builder -m
 echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
@@ -13,6 +14,6 @@ EOM
 
 pacman-key --init
 pacman -Sy --noconfirm && pacman -S --noconfirm archlinuxcn-keyring
-pacman -Syu --noconfirm yay
+pacman -Syu --noconfirm yay "$preinstall_pkgs"
 
 sudo --set-home -u builder yay -S --noconfirm --builddir=./ "$pkgname"

3530d98087c8f0cd2ec5f0ce589f3dfb16e0b5fc
Community <61367732+HilebertSpace@users.noreply.github.com>
2023-12-07 11:56:09 +0800

    Update build.yml (#8)
    * Update build.yml
    
    Signed-off-by: Community <61367732+HilebertSpace@users.noreply.github.com>
    
    * Update PKGBUILD
    
    Signed-off-by: Community <61367732+HilebertSpace@users.noreply.github.com>
    
    * Update PKGBUILD
    
    Signed-off-by: Community <61367732+HilebertSpace@users.noreply.github.com>
    
    * Update PKGBUILD
    
    Signed-off-by: Community <61367732+HilebertSpace@users.noreply.github.com>
    
    * Update PKGBUILD
    
    Signed-off-by: Community <61367732+HilebertSpace@users.noreply.github.com>
    
    * Update PKGBUILD
    
    Signed-off-by: Community <61367732+HilebertSpace@users.noreply.github.com>
    
    * Update PKGBUILD
    
    Signed-off-by: Community <61367732+HilebertSpace@users.noreply.github.com>
    
    * Update entrypoint.sh
    
    Signed-off-by: Community <61367732+HilebertSpace@users.noreply.github.com>
    
    * Update action.yml
    
    Signed-off-by: Community <61367732+HilebertSpace@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Community <61367732+HilebertSpace@users.noreply.github.com>

--- create-db-and-upload-action/entrypoint.sh
+++ create-db-and-upload-action/entrypoint.sh
@@ -12,6 +12,8 @@ if [ ! -f ~/.config/rclone/rclone.conf ]; then
 
     echo "client_id=$RCLONE_ONEDRIVE_CLIENT_ID" >> ~/.config/rclone/rclone.conf
     echo "client_secret=$RCLONE_ONEDRIVE_CLIENT_SECRET" >> ~/.config/rclone/rclone.conf
+    echo "auth_url=$RCLONE_ONEDRIVE_AUTH_URL" >> ~/.config/rclone/rclone.conf
+    echo "token_url=$RCLONE_ONEDRIVE_TOKEN_URL" >> ~/.config/rclone/rclone.conf
     echo "region=$RCLONE_ONEDRIVE_REGION" >> ~/.config/rclone/rclone.conf
     echo "drive_type=$RCLONE_ONEDRIVE_DRIVE_TYPE" >> ~/.config/rclone/rclone.conf
     echo "token=$RCLONE_ONEDRIVE_TOKEN" >> ~/.config/rclone/rclone.conf

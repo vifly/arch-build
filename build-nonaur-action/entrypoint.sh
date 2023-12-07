54e38465587f1261bcc682eed2fa6b9656694e92
Community <61367732+HilebertSpace@users.noreply.github.com>
2023-12-07 09:25:35 +0800

    Update entrypoint.sh (#6)
    * Update entrypoint.sh
    
    Signed-off-by: Community <61367732+HilebertSpace@users.noreply.github.com>
    
    * Update entrypoint.sh
    
    Signed-off-by: Community <61367732+HilebertSpace@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Community <61367732+HilebertSpace@users.noreply.github.com>

--- build-nonaur-action/entrypoint.sh
+++ build-nonaur-action/entrypoint.sh
@@ -87,7 +87,7 @@ for PKGFILE in "${PKGFILES[@]}"; do
 	RELPKGFILE="$(realpath --relative-base="$BASEDIR" "$PKGFILE")"
 	# Caller arguments to makepkg may mean the pacakge is not built
 	if [ -f "$PKGFILE" ]; then
-		echo "::set-output name=pkgfile$i::$RELPKGFILE"
+		echo "pkgfile$i=$RELPKGFILE" >> $GITHUB_OUTPUT
 	else
 		echo "Archive $RELPKGFILE not built"
 	fi

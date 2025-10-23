f7bd50238b7ca56a39e9af425d7348d249a445cd
Vifly <viflythink@gmail.com>
2025-10-24 01:34:03 +0800

    fix can't find dir when pkgbase != pkgname

--- build-aur-action/entrypoint.sh
+++ build-aur-action/entrypoint.sh
@@ -6,19 +6,18 @@ useradd builder -m
 echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 chmod -R a+rw .
 
+cat << EOM >> /etc/pacman.conf
+[archlinuxcn]
+Server = https://repo.archlinuxcn.org/x86_64
+EOM
+
 pacman-key --init
+pacman-key --lsign-key "farseerfc@archlinux.org"
+pacman -Sy --noconfirm && pacman -S --noconfirm archlinuxcn-keyring
 pacman -Syu --noconfirm archlinux-keyring
-install-yay(){
-  pacman -S --needed --noconfirm base-devel
-  sudo --set-home -u builder git clone https://aur.archlinux.org/yay-bin.git buildyay
-  cd buildyay
-  sudo --set-home -u builder makepkg -si --noconfirm
-  cd ..
-  rm -rf buildyay
-}
-install-yay
+pacman -S --noconfirm yay
 if [ ! -z "$INPUT_PREINSTALLPKGS" ]; then
-    pacman -Su --noconfirm "$INPUT_PREINSTALLPKGS"
+    pacman -S --noconfirm "$INPUT_PREINSTALLPKGS"
 fi
 
 sudo --set-home -u builder yay -S --noconfirm --builddir=./ "$pkgname"

16c89b2d3cca5ca8523faf6dea659d961279a908
Sving1024 <sving1024@outlook.com>
2026-01-16 01:50:39 +0800

    import gpg key

--- sync-db/entrypoint.sh
+++ sync-db/entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 set -e
 
+echo "::group::Importing GPG key"
+if [ ! -z "$gpg_key" ]; then
+    echo "$gpg_key" | gpg --import
+fi
+echo "::endgroup::"
+
 init_path=$PWD
 python3 $init_path/sync-db/sync_database.py
